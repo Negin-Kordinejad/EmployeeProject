b3a52f5c1d1f291160680a239976af3dade273b6
Negin <krd.negin@gmail.com>
2023-08-25 12:45:42 +1000

    refactor program service.

--- ParentChildHierarchyProject/Program.cs
+++ ParentChildHierarchyProject/Program.cs
@@ -1,24 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using ParentChildHierarchyProject.Business;
 using ParentChildHierarchyProject.Data.Migrations;
 
 
 
 var builder = CreateHostBuilder(args).Build();
-
 var service = builder.Services.GetRequiredService<IEmployeeHierarchyBusiness>();
+
 var employees = EmployeeDataProvider.GetNames();
 employees.ForEach(GetHierarchy);
 
 void GetHierarchy(string employeName)
 {
     Console.WriteLine($"{employeName} descendants :  ");
-
     var hierarchy = service.GetDescendants(employeName, EmployeeDataProvider.GetHierarchy());
 
-    // var hierarchy = service.GetAncestors(employeName, EmployeeDataProvider.GetHierarchy());
-    // var hierarchy = service.GetFullHierarchy(employeName, EmployeeDataProvider.GetHierarchy());
+    //Console.WriteLine($"{employeName} ancestors :  ");
+    //var hierarchy = service.GetAncestors(employeName, EmployeeDataProvider.GetHierarchy());
+
+    //Console.WriteLine($"{employeName} full hierarchy :  ");
+    //var hierarchy = service.GetFullHierarchy(employeName, EmployeeDataProvider.GetHierarchy());
 
     foreach (var employee in hierarchy)
     {
